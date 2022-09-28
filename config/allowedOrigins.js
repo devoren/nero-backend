ae1359e0ff85e0e02061fac2b5ee4f36accda02e
DevOre <oren.nurkeldi@yandex.kz>
2022-09-29 02:43:18 +0600

    Fixed allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,9 @@
 // CORS (CROSS ORIGIN RESOURCE SHARING) to apply the middleware
 // list of sites that have access to your server
-const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:3000'];
+const allowedOrigins = [
+	'http://127.0.0.1:5500',
+	'http://localhost:3000',
+	'https://nero-frontend.vercel.app',
+];
 
 module.exports = allowedOrigins;
--- middleware/storage.js
+++ middleware/storage.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
 const multer = require('multer');
 
 // where we store files
 const storage = multer.diskStorage({
 	destination: (_, __, cb) => {
+		if (!fs.existsSync('uploads')) {
+			fs.mkdirSync('uploads');
+		}
 		cb(null, 'uploads');
 	},
 	filename: (_, file, cb) => {
