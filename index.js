051913473d315fbf21b3fe15f772ae2a6327d44b
DevOre <oren.nurkeldi@yandex.kz>
2022-11-24 23:46:18 +0600

    Added Cache-Control

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
-const connectDB = require('./config/dbConn');
-const credentials = require('./middleware/credentials');
-const corsOptions = require('./config/corsOptions');
+const connectDB = require("./config/dbConn");
+const credentials = require("./middleware/credentials");
+const corsOptions = require("./config/corsOptions");
 
-const { logger } = require('./middleware/logEvents');
-const errorHandler = require('./middleware/errorHandler');
-const verifyJWT = require('./middleware/verifyJWT');
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
 
-const postController = require('./controllers/postController');
-const commentController = require('./controllers/commentController');
+const postController = require("./controllers/postController");
+const commentController = require("./controllers/commentController");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -42,34 +42,48 @@ app.use(express.json());
 app.use(cookieParser());
 
 // serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/uploads', express.static('uploads'));
+app.use(
+	"/",
+	express.static(path.join(__dirname, "/public"), {
+		cacheControl: true,
+		maxAge: "365d",
+		immutable: true,
+	})
+);
+app.use(
+	"/uploads",
+	express.static("uploads", {
+		cacheControl: true,
+		maxAge: "365d",
+		immutable: true,
+	})
+);
 
-app.use('/auth', require('./routes/auth'));
-app.use('/refresh', require('./routes/refresh'));
-app.use('/logout', require('./routes/logout'));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 // have access without access token
-app.get('/posts', postController.getAllPosts);
-app.get('/posts/comments', commentController.getLastComments);
-app.get('/posts/tags', postController.getLastTags);
-app.get('/posts/:id', postController.getPost);
-app.get('/posts/:postId/comments', commentController.getAllComments);
-app.get('/tags/:tag', postController.getTag);
-app.use('/upload', require('./routes/api/upload'));
+app.get("/posts", postController.getAllPosts);
+app.get("/posts/comments", commentController.getLastComments);
+app.get("/posts/tags", postController.getLastTags);
+app.get("/posts/:id", postController.getPost);
+app.get("/posts/:postId/comments", commentController.getAllComments);
+app.get("/tags/:tag", postController.getTag);
+app.use("/upload", require("./routes/api/upload"));
 
 // verify JWT
 app.use(verifyJWT);
 
 // Now we have access
-app.use('/auth/me', require('./routes/api/user'));
-app.use('/users', require('./routes/api/users'));
-app.use('/posts', require('./routes/api/posts'));
+app.use("/auth/me", require("./routes/api/user"));
+app.use("/users", require("./routes/api/users"));
+app.use("/posts", require("./routes/api/posts"));
 
 app.use(errorHandler);
 
-mongoose.connection.once('open', () => {
-	console.log('Connected to MongoDB');
+mongoose.connection.once("open", () => {
+	console.log("Connected to MongoDB");
 	app.listen(PORT, () => {
 		console.log(`Server running on port: ${PORT}`);
 	});
