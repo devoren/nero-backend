88a228d3208f923ad3ae4a75f272dbf03ad6adfe
DevOre <oren.nurkeldi@yandex.kz>
2022-12-07 19:10:04 +0600

    Updated allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,10 @@
 // CORS (CROSS ORIGIN RESOURCE SHARING) to apply the middleware
 // list of sites that have access to your server
 const allowedOrigins = [
-	'http://127.0.0.1:5500',
-	'http://localhost:3000',
-	'https://nero-frontend.vercel.app',
+	"http://127.0.0.1:5500",
+	"http://localhost:3000",
+	"https://nero-frontend.vercel.app",
+	"https://devoren.vercel.app",
 ];
 
 module.exports = allowedOrigins;
